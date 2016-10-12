be2ee93080b77c336801487d2f0390e9bfbefce3
Naalik <lormir.panther@gmail.com>
2016-10-12 16:49:37 +0200

    Commit exo site2 html css js

--- public_html/js/javascript.js
+++ public_html/js/javascript.js
@@ -4,11 +4,27 @@
  * and open the template in the editor.
  */
 var reponse = Math.round((Math.random()*100)+1);
+var essai = 0;
 alert(reponse);
 do{
 var texte = prompt('entrer un chiffre entre 1 et 100');
+essai += 1;
 if(texte == reponse){
     alert('Win !');
+    switch(essai){
+        case 1:
+            alert('Génial !');
+            break;
+        case 2:
+            alert('Bien !');
+            break;
+        case 3:
+            alert('Cool, bro.');
+            break;
+        default:
+            alert('Bof.');
+            break;
+    }
 }else{
     if( texte > reponse){
         alert('trop grand !');
